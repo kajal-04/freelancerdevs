60f5167c56408c5a0910ee00a179572a25056137
kajal-04 <kajalkumari540651@gmail.com>
2025-03-24 02:18:36 +0530

    fixed typo

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -105,7 +105,7 @@ export default function ContactPage() {
 
     if (!new RegExp(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/).test(formData.email)) {
       toast({
-        title: "Inalid email address",
+        title: "Invalid email address",
         variant: "destructive",
       });
       setIsSubmitting(false)
